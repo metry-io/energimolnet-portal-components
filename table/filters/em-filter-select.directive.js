26768be471ae1efd03517d62811e3e41c7fe3ca0
Torsten Freyhall <torsten@freyhall.se>
2015-05-12 13:15:12 +0200

    fix: em-filter-select: Added default option to prevent angular errors with transcluding options

--- table/filters/em-filter-select.directive.js
+++ table/filters/em-filter-select.directive.js
@@ -6,7 +6,7 @@ angular.module('portal-components')
     replace: true,
     transclude: true,
     template: '<div class="form-group" ng-class="{\'has-success\': filter.length}">' +
-                '<select class="form-control" ng-model="filter" ng-transclude></select>' +
+                '<select class="form-control" ng-model="filter" ng-transclude><option value=""></option></select>' +
               '</div>',
     scope: {
       key: '@emKey',
