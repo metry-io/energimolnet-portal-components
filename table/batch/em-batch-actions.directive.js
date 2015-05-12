d7c38a6a05c831031fffb6f0598b7f58aa569994
Torsten Freyhall <torsten@freyhall.se>
2015-05-12 14:51:16 +0200

    fix: batch-actions: Updated template for new structure

--- table/batch/em-batch-actions.directive.js
+++ table/batch/em-batch-actions.directive.js
@@ -5,12 +5,12 @@ angular.module('portal-components')
     restrict: 'E',
     replace: true,
     transclude: true,
-    template: '<div class="em-batch-actions">' +
-                '<div class="dropdown" dropdown>' +
-                  '<button class="btn btn-primary dropdown-toggle" dropdown-toggle style="padding: 6px">' +
-                    '<input type="checkbox" ng-model="batchMgr.allSelected" ng-change="change()">' +
-                    '<span class="caret" style="margin-left: 10px"></span>' +
-                  '<ul class="dropdown-menu" ng-transclude></ul>',
+    template: '<div class="dropdown" dropdown>' +
+                '<button class="btn btn-primary dropdown-toggle" dropdown-toggle style="padding: 6px">' +
+                  '<input type="checkbox" ng-model="batchMgr.allSelected" ng-change="change()">' +
+                  '<span class="caret" style="margin-left: 10px"></span>' +
+                '<ul class="dropdown-menu" ng-transclude></ul>' +
+              '</div>',
     scope: {},
     require: '^emResourceTable',
     link: function(scope, element, attrs, resourceTableCtrl) {
