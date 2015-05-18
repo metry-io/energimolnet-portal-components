ce771f554a13502e8f07533ba926ef513589cf11
Torsten Freyhall <torsten@freyhall.se>
2015-05-18 16:36:38 +0200

    bugfix: em-resource-table: Fixed dependency injection bug

--- table/em-resource-table.directive.js
+++ table/em-resource-table.directive.js
@@ -7,13 +7,13 @@ angular.module('portal-components')
       resMgr: '=emResourceManager',
       batchMgr: '=?emBatchManager'
     },
-    controller: function($scope) {
+    controller: ['$scope', function($scope) {
       this.resMgr = $scope.resMgr;
 
       if ($scope.batchMgr) {
         this.batchMgr = $scope.batchMgr;
         this.resMgr.batchMgr = this.batchMgr;
       }
-    }
+    }]
   };
 });
