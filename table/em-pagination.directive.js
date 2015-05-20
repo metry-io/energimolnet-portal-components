78629fe3a24e4a48e27efdcb7a14f7b11b34a291
Torsten Freyhall <torsten@freyhall.se>
2015-05-20 12:10:12 +0200

    fix: em-pagination: Updated english text

--- table/em-pagination.directive.js
+++ table/em-pagination.directive.js
@@ -5,7 +5,7 @@ angular.module('portal-components')
     restrict: 'E',
     template: '<div class="text-center" ng-show="!resMgr.loading">' +
               '<pagination boundary-links="true" previous-text="&lsaquo;" next-text="&rsaquo;" first-text="&laquo;" last-text="&raquo;" ng-model="resMgr.pagination.page" ng-change="resMgr.updatePage()" total-items="resMgr.pagination.count" items-per-page="resMgr.pagination.limit" max-size="10"></pagination>' +
-              '<p ng-if=":: ctrl.lang === \'en\'">Showing from {{ resMgr.pagination.from }} to {{ resMgr.pagination.to }} of {{ resMgr.pagination.count }} matching</p>' +
+              '<p ng-if=":: ctrl.lang === \'en\'">Showing from {{ resMgr.pagination.from }} to {{ resMgr.pagination.to }} of {{ resMgr.pagination.count }} matches</p>' +
               '<p ng-if=":: ctrl.lang === \'sv\'">Visar från {{ resMgr.pagination.from }} till {{ resMgr.pagination.to }} av {{ resMgr.pagination.count }} matchande</p>' +
               '</div>',
     scope: {
