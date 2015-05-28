adabb8f0c85e86cd33b20b3192773ab058d133a4
Torsten Freyhall <torsten@freyhall.se>
2015-05-28 11:41:54 +0200

    fix: em-pagination: Pagination is now hidden when there are zero results

--- table/em-pagination.directive.js
+++ table/em-pagination.directive.js
@@ -3,7 +3,7 @@ angular.module('portal-components')
 .directive('emPagination', function() {
   return {
     restrict: 'E',
-    template: '<div class="text-center" ng-show="!resMgr.loading">' +
+    template: '<div class="text-center" ng-show="!resMgr.loading && resMgr.data.length > 0">' +
               '<pagination boundary-links="true" previous-text="&lsaquo;" next-text="&rsaquo;" first-text="&laquo;" last-text="&raquo;" ng-model="resMgr.pagination.page" ng-change="resMgr.updatePage()" total-items="resMgr.pagination.count" items-per-page="resMgr.pagination.limit" max-size="10"></pagination>' +
               '<p ng-if=":: ctrl.lang === \'en\'">Showing from {{ resMgr.pagination.from }} to {{ resMgr.pagination.to }} of {{ resMgr.pagination.count }} matches</p>' +
               '<p ng-if=":: ctrl.lang === \'sv\'">Visar från {{ resMgr.pagination.from }} till {{ resMgr.pagination.to }} av {{ resMgr.pagination.count }} matchande</p>' +
