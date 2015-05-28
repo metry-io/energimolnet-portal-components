ce2ac77bff05db195295fb1c3f57feb2a819c91c
Torsten Freyhall <torsten@freyhall.se>
2015-05-28 16:45:39 +0200

    hack: Another attempt to fix ie9 select issue

--- table/filters/em-filter-select.directive.js
+++ table/filters/em-filter-select.directive.js
@@ -1,35 +1,45 @@
 angular.module('portal-components')
 
-.directive('emFilterSelect', function() {
-  return {
-    restrict: 'E',
-    replace: true,
-    transclude: true,
-    template: '<div class="form-group" ng-class="{\'has-success\': filter.length && filter !== default}">' +
-                '<select class="form-control" ng-model="filter" ng-transclude><option value=""></option></select>' +
-              '</div>',
-    scope: {
-      key: '@emKey',
-      default: '=emDefaultValue'
-    },
-    require: '^emResourceTable',
-    link: function(scope, element, attrs, resourceTableCtrl) {
-      var resMgr = resourceTableCtrl.resMgr;
+.directive('emFilterSelect', [
+  '$timeout',
+  function($timeout) {
+    return {
+      restrict: 'E',
+      replace: true,
+      transclude: true,
+      template: '<div class="form-group" ng-class="{\'has-success\': filter.length && filter !== default}">' +
+                  '<select class="form-control" ng-model="filter" ng-transclude><option value=""></option></select>' +
+                '</div>',
+      scope: {
+        key: '@emKey',
+        default: '=emDefaultValue'
+      },
+      require: '^emResourceTable',
+      link: function(scope, element, attrs, resourceTableCtrl) {
+        var resMgr = resourceTableCtrl.resMgr;
 
-      scope.filter = resMgr.filter[scope.key];
-      scope.resMgrFilter = resMgr.filter;
+        scope.filter = resMgr.filter[scope.key];
+        scope.resMgrFilter = resMgr.filter;
 
-      scope.$watch('filter', function(newFilter, oldFilter) {
-        if (newFilter === oldFilter) return;
+        scope.$watch('filter', function(newFilter, oldFilter) {
+          if (newFilter === oldFilter) return;
 
-        resMgr.setFilterOption(scope.key, newFilter);
-      });
+          resMgr.setFilterOption(scope.key, newFilter);
+        });
 
-      scope.$watch('resMgrFilter.' + scope.key, function(newFilter, oldFilter) {
-        if (newFilter === oldFilter) return;
+        scope.$watch('resMgrFilter.' + scope.key, function(newFilter, oldFilter) {
+          if (newFilter === oldFilter) return;
 
-        scope.filter = newFilter;
-      });
-    }
-  };
-});
+          scope.filter = newFilter;
+        });
+
+        // IE9 hack
+        $timeout(function() {
+          console.log('Refreshed');
+          element.addClass('ng-hide');
+          element.removeClass('ng-hide');
+        }, 500);
+      }
+    };
+  }
+]);
