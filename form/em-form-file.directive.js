fbc0c4d4c54299c08113fbe6736e18d81eab33ea
Lennaert van Dijke <Lennaert@Lennaert.nu>
2015-12-17 16:11:54 +0100

    Upload URL endpoint updated

--- form/em-form-file.directive.js
+++ form/em-form-file.directive.js
@@ -1,8 +1,8 @@
 angular.module('app.directives')
 
 .directive('emFormFile', [
-  '$upload',
-  function($upload) {
+  '$upload', 'apiBaseUrl',
+  function($upload, apiBaseUrl) {
     return {
       template: '<div ng-class="{&quot;has-error&quot;: errors}" class="form-group"><label class="col-sm-2 control-label">{{ label }}</label><div class="col-sm-10"><input type="file" ng-file-select="uploadFiles($files)"/><div ng-file-drop="uploadFiles($files)" ng-file-drag-over-class="drag-over" ng-file-drop-available="dropSupported=true" class="drop-box"></div><div ng-show="dropSupported &amp;&amp; !model">Drop files here</div><img ng-show="showThumbnail(model)" ng-src="{{ model }"/><a ng-show="model" ng-href="{{ model }}" target="_blank">{{ model }}</a><button ng-click="upload.abort()" class="btn btn-default btn-xs">Cancel Upload</button><button ng-click="deleteFile(model)" ng-show="model" class="btn btn-danger btn-xs">Delete file</button><p ng-repeat="msg in errors" class="help-block">{{ msg }}</p></div></div>',
       restrict: 'E',
@@ -16,7 +16,7 @@ angular.module('app.directives')
         scope.uploadFiles = function(files) {
           files.forEach(function(file) {
             scope.upload = $upload.upload({
-              url: 'assets',
+              url: apiBaseUrl + 'api/2.0/assets',
               method: 'POST',
               file: file,
               fileFormDataName: 'media'
